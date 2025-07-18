1b1ca70f25c83269ea4e150004e6d23cb9bf14e6
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-18 14:40:37 -0300

    aula Expressão mat aula 29

---
+++ ud_29_exp_mat.py
@@ -0,0 +1,25 @@
+# Expressão matemática é combinação de  números, operadores, variáveis e símbolos gráficos
+#linealizar = deixar de froma que posso usar no cód
+# numero = 0 a 9
+# operadores = +(soma) -(subtração) *(multiplicação) /(divisão) %(resto) //(soma valor inteira) **(exponecniação)
+# variáveis = y, x
+# símbolo gráfico = ()
+
+x = 15
+y = 2
+
+soma = x + y
+subtra = x - y
+multi = x * y
+div = x / y
+resto = x % y
+div_int = x // y
+elevado = x**y
+
+print(soma)
+print(subtra)
+print(multi)
+print(div)
+print(resto)
+print(div_int)
+print(elevado)
